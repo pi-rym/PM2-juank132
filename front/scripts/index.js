aa8bf2557d7572afcc4d380f8141911f37eeeba2
juank132 <juancarlos_castillo@hotmail.com.ar>
2024-03-06 03:38:01 -0300

    script optimizado

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,35 +1,25 @@
 
+console.log(tempData);
 
-$.get(`https://students-api.2.us-1.fl0.io/movies`, function(data) {
-  
-  console.log(tempData);
+const tarjetas = document.querySelectorAll('.tarjeta_pelicula')
 
-  const tarjetas = document.querySelectorAll('.tarjeta_pelicula')
+
+$.get(`https://students-api.2.us-1.fl0.io/movies`, (data) => {
   
-  function iterarImagen(tar, i) {
-      const e = data[i];
+  function iterarImagen(tar,i) {
+        const e = data[i];
   
-      //ESTE ES MI MACHETE VISUAL
-  
-      const tituloT = e.title;
-      const directorT = e.director;
-      const rateT= e.rate;
-      const yearT=  e.year;
-      const genreT= e.genre;
-      const durationT= e.duration;
-      const img = e.poster;
-      
-  
-      tar.style.backgroundImage = `url('${img}')`;
-      tar.querySelector('.title').append(tituloT);
-      tar.querySelector('.director').append(directorT); 
-      tar.querySelector('.rate').append(rateT); 
-      tar.querySelector('.year').append(yearT) ;
-      tar.querySelector('.genre').append(genreT); 
-      tar.querySelector('.duration').append(durationT);
-  }
-  
-  
+        tar.style.backgroundImage = `url('${e.poster}')`;
+        tar.querySelector('.title').append(e.title);
+        tar.querySelector('.director').append(e.director); 
+        tar.querySelector('.rate').append(e.rate); 
+        tar.querySelector('.year').append(e.year) ;
+        tar.querySelector('.genre').append( e.genre); 
+        tar.querySelector('.duration').append(e.duration);
+
+    }
+
+    
   tarjetas.forEach(iterarImagen);
 
 
