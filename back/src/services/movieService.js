f3dd1b74d1d98df25c1d9d160aa087af102c2f8b
juank132 <juancarlos_castillo@hotmail.com.ar>
2024-03-18 03:22:13 -0300

    mongoDB

--- back/src/services/movieService.js
+++ back/src/services/movieService.js
@@ -1,14 +1,14 @@
 const axios = require("axios");
 const promesa = axios.get(`https://students-api.up.railway.app/movies`);
 
-const ejec = 
-    
-    promesa.then((res) => {
-    return res.data
-   }).catch((err) => {
-    return err
-   })
+const ejec = promesa.then((res) => {
+         return res.data
+        }).catch((err) => {
+         return err
+        })
 
+    
+ 
 
 module.exports = ejec;
 
